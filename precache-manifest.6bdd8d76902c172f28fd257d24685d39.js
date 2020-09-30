d0f300a332fd3c3ab45d696192dc90451117220e
jujomago <juan.mancilla@avantica.net>
2020-09-30 18:23:23 -0400

    Updates

---
+++ precache-manifest.6bdd8d76902c172f28fd257d24685d39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0779a63150efa1c89d182eb69dce9c6e",
+    "url": "/challenge-properties/index.html"
+  },
+  {
+    "revision": "2b73d9115a2faeafd2f3",
+    "url": "/challenge-properties/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "0750d2f41e6b1eef83ae",
+    "url": "/challenge-properties/static/css/main.f07094ec.chunk.css"
+  },
+  {
+    "revision": "2b73d9115a2faeafd2f3",
+    "url": "/challenge-properties/static/js/2.be2cda09.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/challenge-properties/static/js/2.be2cda09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0750d2f41e6b1eef83ae",
+    "url": "/challenge-properties/static/js/main.9fb13972.chunk.js"
+  },
+  {
+    "revision": "60446f8f2fae1db19491",
+    "url": "/challenge-properties/static/js/runtime-main.af8b7517.js"
+  }
+]);
